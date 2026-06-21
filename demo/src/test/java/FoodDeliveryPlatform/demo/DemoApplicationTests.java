02e699fb0c7708172820e7593e1ede25f8dfc9a6
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 09:30:00 +0400

    adding dependencies

---
+++ demo/src/test/java/FoodDeliveryPlatform/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package FoodDeliveryPlatform.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
