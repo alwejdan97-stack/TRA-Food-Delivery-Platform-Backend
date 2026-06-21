c0f0b9995be1b00320f2cd097c23d7e2d54fa072
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 09:42:59 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Customer.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Customer.java
@@ -1,4 +1,23 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.dialect.function.array.AbstractArrayTrimFunction;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Customer")
 public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private int loyaltyPoints;
+    private String customerCode;
 }
--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
@@ -1,11 +1,4 @@
 package FoodDeliveryPlatform.demo.Entities;
 
 public class Review {
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
-    private String passwordHash;
-    private int loyaltyPoints;
-    private String customerCode;
 }
