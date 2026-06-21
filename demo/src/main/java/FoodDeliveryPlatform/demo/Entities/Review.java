0b4d21e7c5fc3946ca23df9345d42ef71a265099
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 09:40:19 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
@@ -1,4 +1,11 @@
 package FoodDeliveryPlatform.demo.Entities;
 
 public class Review {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private int loyaltyPoints;
+    private String customerCode;
 }
