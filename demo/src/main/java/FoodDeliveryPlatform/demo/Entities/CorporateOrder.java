1b9a91eea5737afecac536fa6dcb7c7e26416266
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:11:40 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/CorporateOrder.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/CorporateOrder.java
@@ -1,4 +1,26 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "CorporateOrder")
 public class CorporateOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String corporateCode;
+    private String companyName;
+    private double costCenter;
+    private LocalDate orderDate;
+    private Boolean status;
+    private double totalAmount;
 }
