a2885f8a63d23a762eb6f12dc907f7a06f4646fc
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:26:40 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Payment.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Payment.java
@@ -1,4 +1,12 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import java.time.LocalDateTime;
+
 public class Payment {
+    private long id;
+    private String paymentMethod;
+    private String status;
+    private double amount;
+    private String transactionRef;
+    private LocalDateTime processedAt;
 }
