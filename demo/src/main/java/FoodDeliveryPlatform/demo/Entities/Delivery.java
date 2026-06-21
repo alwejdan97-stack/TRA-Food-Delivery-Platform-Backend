e3d55c8d4ecf2f80aa79bfedb37e95ca413bd16f
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:21:40 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Delivery.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Delivery.java
@@ -1,4 +1,13 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Delivery {
+    private long id;
+    private String trackingCode;
+    private Boolean status;
+    private LocalDateTime assignedAt;
+    private LocalDateTime pickedUpAt;
+    private LocalDateTime deliveredAt;
 }
