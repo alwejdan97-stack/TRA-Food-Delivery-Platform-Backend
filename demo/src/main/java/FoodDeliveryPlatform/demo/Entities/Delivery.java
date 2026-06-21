fa762b428a87a0100a366de350d23565cc185580
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:22:44 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Delivery.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Delivery.java
@@ -1,13 +1,27 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Delivery")
 public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String trackingCode;
     private Boolean status;
     private LocalDateTime assignedAt;
     private LocalDateTime pickedUpAt;
     private LocalDateTime deliveredAt;
+
 }
