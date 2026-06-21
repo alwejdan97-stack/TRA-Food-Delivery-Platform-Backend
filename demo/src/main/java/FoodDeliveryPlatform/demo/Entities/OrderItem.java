5767138d000b5ad667ca19ad84c670e14187c96d
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:16:53 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/OrderItem.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/OrderItem.java
@@ -1,7 +1,20 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "OrderItem")
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private int quantity;
     private double unitPrice;
     private double totalPrice;
