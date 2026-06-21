6e0ce99345a36f7679a9f445ebf32f9e8253fbf6
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:30:04 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
@@ -1,4 +1,11 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import java.time.LocalDateTime;
+
 public class Review {
+    private long id;
+    private String targetType;
+    private String rating;
+    private String comment;
+    private LocalDateTime createdAt;
 }
