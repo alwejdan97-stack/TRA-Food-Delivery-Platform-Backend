f4ed518533d8e84d2d17b8ad086ac3c285239dd4
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:30:59 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Review.java
@@ -1,11 +1,26 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Review")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String targetType;
     private String rating;
     private String comment;
     private LocalDateTime createdAt;
+
+
 }
