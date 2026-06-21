d07797ee1e6c835600ba3cc5f677a28062df6c51
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:50:19 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/MenuItem.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/MenuItem.java
@@ -1,7 +1,20 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "MenuItem")
 public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
     private String description;
     private double price;
