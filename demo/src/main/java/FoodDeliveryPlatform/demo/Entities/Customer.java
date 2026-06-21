e43cf80027b43b74b8e78f94e914d24bf0d123d8
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 09:59:39 +0400

    adding relationships

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Customer.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Customer.java
@@ -1,11 +1,12 @@
 package FoodDeliveryPlatform.demo.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.dialect.function.array.AbstractArrayTrimFunction;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -13,11 +14,27 @@ import org.hibernate.dialect.function.array.AbstractArrayTrimFunction;
 @AllArgsConstructor
 @Table(name = "Customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long customerId;
+
     private String firstName;
     private String lastName;
+
+    @Column(unique = true,nullable = false)
     private String email;
+
     private String phone;
     private String passwordHash;
     private int loyaltyPoints;
     private String customerCode;
+
+    @OneToMany(mappedBy = "Customer")
+    private List<CustomerAddress> customerAddresses;
+
+    @OneToMany(mappedBy = "Customer")
+    private List<Order> orders;
+
+    @OneToMany(mappedBy = "Customer")
+    private List<Review> reviews;
 }
