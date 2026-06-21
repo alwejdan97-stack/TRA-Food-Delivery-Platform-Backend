45be81fedc01c310127aca550a64dcdbacf188d0
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:01:09 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Order.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Order.java
@@ -1,4 +1,22 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String orderCode;
+    private LocalDate orderDate;
+    private Boolean status;
+    private double subtotal;
+    private double deliveryFee;
+    private double discountAmount;
+    private double totalAmount;
+    private String deliveryNotes;
 }
