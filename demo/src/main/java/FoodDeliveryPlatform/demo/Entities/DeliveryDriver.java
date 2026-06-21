f879fa0b49b18b4b1746f438dfc82a54f36c22a6
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:09:10 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/DeliveryDriver.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/DeliveryDriver.java
@@ -1,4 +1,16 @@
 package FoodDeliveryPlatform.demo.Entities;
 
 public class DeliveryDriver {
+    private long driverId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private String driverCode;
+    private String vehicleType;
+    private String vehiclePlate;
+    private double currentLat;
+    private double currentLng;
+    private Boolean isOnline;
 }
