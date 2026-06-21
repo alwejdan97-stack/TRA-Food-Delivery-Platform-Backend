ca7a746420bed885886890e96e30f35ed30d8372
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:48:48 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/MenuItem.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/MenuItem.java
@@ -1,4 +1,11 @@
 package FoodDeliveryPlatform.demo.Entities;
 
 public class MenuItem {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private Boolean isAvailable;
+    private Boolean isVegetarian;
+    private double calories;
 }
