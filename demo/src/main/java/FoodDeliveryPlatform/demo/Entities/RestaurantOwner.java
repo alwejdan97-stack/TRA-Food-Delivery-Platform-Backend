8b798b69df4a138b33f5c84aad87b6348cff485d
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:22:33 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/RestaurantOwner.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/RestaurantOwner.java
@@ -1,7 +1,20 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "RestaurantOwner")
 public class RestaurantOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String firstName;
     private String lastName;
     private String email;
