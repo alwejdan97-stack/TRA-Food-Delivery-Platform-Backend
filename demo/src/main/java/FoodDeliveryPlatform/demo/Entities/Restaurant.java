3a552576c723a0497712fbfc2950407bbc509caf
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:41:07 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Restaurant.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Restaurant.java
@@ -1,4 +1,15 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import java.time.LocalTime;
+
 public class Restaurant {
+    private long restaurantId;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+    private double minOrderAmount;
+    private double deliveryFee;
+    private Boolean acceptingOrders;
 }
