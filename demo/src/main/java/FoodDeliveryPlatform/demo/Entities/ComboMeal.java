3cda7f8876cc5b2a9e5984d19f9ea1c87d1193bd
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:53:52 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/ComboMeal.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/ComboMeal.java
@@ -1,4 +1,9 @@
 package FoodDeliveryPlatform.demo.Entities;
 
 public class ComboMeal {
+    private long id;
+    private String comboName;
+    private String description;
+    private double totalPrice;
+    private Boolean isAvailable;
 }
