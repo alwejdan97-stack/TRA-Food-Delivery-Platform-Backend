a4db6f6b01020444d013ce43b31bcea560ea9094
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:27:44 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Payment.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/Payment.java
@@ -1,8 +1,21 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Payment")
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String paymentMethod;
     private String status;
