aaf84e587a77c8ae734d81d63f3857733fa9edb9
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:10:25 +0400

    adding annotations

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/DeliveryDriver.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/DeliveryDriver.java
@@ -1,7 +1,20 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "DeliveryDriver")
 public class DeliveryDriver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long driverId;
+
     private String firstName;
     private String lastName;
     private String email;
