4a240a68ca8bc679953caf7a25185535135b57be
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 11:15:43 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/OrderItem.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/OrderItem.java
@@ -1,4 +1,9 @@
 package FoodDeliveryPlatform.demo.Entities;
 
 public class OrderItem {
+    private long id;
+    private int quantity;
+    private double unitPrice;
+    private double totalPrice;
+    private String specialInstructions;
 }
