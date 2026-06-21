11787a2288ef9bdbe8054a79dadcea235aa2be30
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:54:59 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/ComboMeal.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/ComboMeal.java
@@ -1,7 +1,20 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ComboMeal")
 public class ComboMeal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String comboName;
     private String description;
     private double totalPrice;
