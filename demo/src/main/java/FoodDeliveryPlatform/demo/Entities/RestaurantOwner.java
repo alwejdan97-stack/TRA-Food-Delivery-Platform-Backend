9da140b2dc3d2567747f1b261ca53cf585f27795
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:20:29 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/RestaurantOwner.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/RestaurantOwner.java
@@ -1,4 +1,11 @@
 package FoodDeliveryPlatform.demo.Entities;
 
 public class RestaurantOwner {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private String businessLicenseCode;
 }
