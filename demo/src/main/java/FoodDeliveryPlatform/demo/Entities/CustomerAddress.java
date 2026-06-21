014af229ebb13939f9e34303f86ee9f880dad234
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-21 10:02:03 +0400

    adding private feilds

--- demo/src/main/java/FoodDeliveryPlatform/demo/Entities/CustomerAddress.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Entities/CustomerAddress.java
@@ -1,4 +1,15 @@
 package FoodDeliveryPlatform.demo.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class CustomerAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long addressId;
+    private String street;
+    private String city;
+    private String building;
+    private Boolean isDefault;
 }
