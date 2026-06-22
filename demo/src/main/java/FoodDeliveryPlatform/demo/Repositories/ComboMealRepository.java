38b95f732b99c949e705583b3d83e67f05b96396
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-22 12:05:31 +0400

    adding repository and query

--- demo/src/main/java/FoodDeliveryPlatform/demo/Repositories/ComboMealRepository.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Repositories/ComboMealRepository.java
@@ -1,5 +1,7 @@
 package FoodDeliveryPlatform.demo.Repositories;
 
+import FoodDeliveryPlatform.demo.Entities.ComboMeal;
+import FoodDeliveryPlatform.demo.Entities.CustomerAddress;
 import FoodDeliveryPlatform.demo.Entities.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,17 +9,17 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface MenuItemRepository extends JpaRepository<MenuItem,Integer> {
-    @Query("SELECT CM FROM MenuItem M WHERE M.Restaurant.id=: id AND M.isActive=TRUE AND M.Restaurant.isActive=TRUE")
-    List<MenuItem>  findByRestaurantId(@Param("id") Integer id);
+public interface ComboMealRepository extends JpaRepository<ComboMeal,Integer> {
+    @Query("SELECT CM FROM ComboMeal CM WHERE CM.Restaurant.id=: id AND CM.isActive=TRUE AND CM.Restaurant.isActive=TRUE")
+    List<MenuItem> findByRestaurantId(@Param("id") Integer id);
 
     @Query("SELECT CM FROM ComboMeal CM WHERE CM.Restaurant.id=: id AND CM.isAvailable=TRUE AND CM.isActive=TRUE AND CM.Restaurant.isActive=TRUE")
     List<MenuItem> findByRestaurantIdAndIsAvailableTrue(Integer id);
 
-    @Query("SELECT M FROM MenuItem M WHERE M.isVegetarian= TRUE AND M.isActive=TRUE")
+    @Query("SELECT CM FROM ComboMeal CM WHERE CM.isVegetarian= TRUE AND CM.isActive=TRUE")
     List<MenuItem> findByIsVegetarianTrue();
 
-    @Query("SELECT M FROM MenuItem M WHERE M.price BETWEEN :min AND :max AND M.isActive=TRUE")
+    @Query("SELECT CM FROM ComboMeal CM WHERE CM.totalPrice BETWEEN :min AND :max AND CM.isActive=TRUE")
     List<MenuItem> findByPriceBetween(@Param("min") Double min, @Param("max") Double max);
 
     @Query("SELECT CM FROM ComboMeal CM JOIN CM.MenuItem M WHERE M.id=:MenuItemId AND CM.isActive=TRUE AND M.isActive=TRUE AND CM.Restaurant.isActive=TRUE")
