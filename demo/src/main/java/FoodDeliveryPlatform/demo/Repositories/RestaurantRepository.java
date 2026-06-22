ccbd2a10710aa667a3e8cf0332be3e915a07292c
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-22 11:30:50 +0400

    adding repository and query

---
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Repositories/RestaurantRepository.java
@@ -0,0 +1,27 @@
+package FoodDeliveryPlatform.demo.Repositories;
+
+import FoodDeliveryPlatform.demo.Entities.Restaurant;
+import FoodDeliveryPlatform.demo.Entities.RestaurantOwner;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant,Integer> {
+    @Query("SELECT R FROM Restaurant R WHERE LOWER(R.cuisineType)=LOWER(:cuisineType) AND R.isActive=TRUE")
+    List<Restaurant> findByCuisineTypeIgnoreCase(@Param("cuisineType") String cuisineType);
+
+    @Query("SELECT R FROM Restaurant R WHERE R.acceptingOrders= TRUE AND R.isActive=TRUE")
+    List<Restaurant> findByAcceptingOrdersTrue();
+
+    @Query("SELECT R FROM Restaurant R WHERE R.deliveryFee<=:deliveryFee AND R.isActive=TRUE")
+    List<Restaurant> findByDeliveryFeeLessThanEqual(@Param("deliveryFee") Double fee);
+
+    @Query("SELECT R FROM Restaurant R WHERE R.RestaurantOwner.id=:id AND R.isActive=TRUE AND R.RestaurantOwner.isActive=TRUE")
+    List<Restaurant> findByRestaurantOwnerId(@Param("id") Integer id);
+
+    @Query("SELECT R FROM Restaurant R WHERE R.name LIKE %:keyword% AND R.isActive=TRUE")
+    List<Restaurant> findRestaurantsByNameKeyword(String keyWord);
+}
