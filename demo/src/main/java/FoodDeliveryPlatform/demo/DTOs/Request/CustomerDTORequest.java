46cb29c803f708a419290c6c76e9ccfd54f485af
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-22 16:05:32 +0400

    creating Customer DTO classes

--- demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Request/CustomerDTORequest.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Request/CustomerDTORequest.java
@@ -1,13 +1,28 @@
 package FoodDeliveryPlatform.demo.DTOs.Request;
 
+import FoodDeliveryPlatform.demo.Entities.Customer;
 import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class CustomerDTORequest {
+    private Integer id;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
     private String passwordHash;
 
-
+    public void applyToEntity(Customer entity){
+        CustomerDTORequest  dto=new CustomerDTORequest();
+        dto.setId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setEmail(entity.getEmail());
+        dto.setPhone(entity.getPhone());
+        //dto.setPasswordHash(entity.getPasswordHash());
+        //return dto;
+    }
 }
--- demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Summary/CustomerDTOSummary.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Summary/CustomerDTOSummary.java
@@ -1,4 +1,28 @@
 package FoodDeliveryPlatform.demo.DTOs.Summary;
 
+import FoodDeliveryPlatform.demo.DTOs.Response.CustomerDTOResponse;
+import FoodDeliveryPlatform.demo.Entities.Customer;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class CustomerDTOSummary {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    //private String passwordHash;
+
+    public static CustomerDTOSummary fromEntity(Customer entity) {
+        CustomerDTOSummary dto = new CustomerDTOSummary();
+        dto.setId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setPhone(entity.getPhone());
+        dto.setEmail(entity.getEmail());
+
+        return dto;
+    }
 }
