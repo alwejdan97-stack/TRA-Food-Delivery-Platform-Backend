1fa8bb5084fff3dbd0788787ef32aeaa6fee08da
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-23 11:30:42 +0400

    creating ErrorResponse classes

--- demo/src/main/java/FoodDeliveryPlatform/demo/Exceptions/ErrorResponse.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Exceptions/ErrorResponse.java
@@ -1,4 +1,23 @@
 package FoodDeliveryPlatform.demo.Exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
+    private LocalDate timestamp;
+    private Integer httpStatus;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse(HttpStatus httpStatus, int value, String internalServerError, String s, String replace) {
+    }
 }
