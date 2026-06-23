5dc6fee8061578dba881dc6b79b51046654a9e5c
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-23 12:53:01 +0400

    creating HelperUtils class

--- demo/src/main/java/FoodDeliveryPlatform/demo/Exceptions/GlobalExceptionHandler.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,67 @@
 package FoodDeliveryPlatform.demo.Exceptions;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @RestControllerAdvice
 @AllArgsConstructor
 public class GlobalExceptionHandler extends RuntimeException{
     public GlobalExceptionHandler(String message){
         super(message);
+    }
+
+    @ExceptionHandler(GenericException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(GenericException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND ,
+                HttpStatus.NOT_FOUND.value(),
+                "RESOURCE NOT FOUND...",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", ""));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse errorDetails = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                "An unexpected error occurred on the server.",
+                request.getDescription(false).replace("uri=", "")
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+    }
+
+
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateResourceException(DuplicateResourceException exception, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        ;
+    }
+
+    @ExceptionHandler(GenericException.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(GenericException exception, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+
+    }
+
+    @ExceptionHandler(InvalidOrderStateException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidOrderStateException(InvalidOrderStateException exception, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+
     }
 }
---
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Utilities/HelperUtils.java
@@ -0,0 +1,65 @@
+package FoodDeliveryPlatform.demo.Utilities;
+
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+public class HelperUtils {
+
+    public static String generateCode(String prefix){}
+
+    public static String generateCode(String prefix, int length){}
+
+    public static Double calculateDistance(double lat1, double lng1, double lat2, double lng2){
+        //convert degree to radians
+        Double dlat=Math.toRadians(lat2-lat1);
+        Double lng=Math.toRadians(lng2-lng1);
+
+        //Raduius for earth in kilometers=6371
+        double radius=6371;
+
+        // Haversine formula
+        Double a=(Math.pow(Math.sin(dlat/2),2)+Math.cos(lat1)*Math.cos(lat2)) * Math.pow(Math.sin(dlat/2),2);
+
+        //calculate Central angle
+        Double c=2* Math.asin(Math.sqrt(a));
+
+        //calculate the result
+        Double result=c*radius;
+
+        return result;
+    }
+
+    public static Double calculateTotal(double subtotal, double fee){
+        Double total=subtotal+fee;
+        return total;
+    }
+
+    public static Double calculateTotal(double subtotal, double fee, double discount){
+        Double total=subtotal+fee-discount;
+        return total;
+    }
+
+    public static String formatCurrency(double amount){
+        NumberFormat format=NumberFormat.getCurrencyInstance();
+        //format.setCurrency(Currency.getInstance(currencyCode));
+        return format.format(amount);
+    }
+
+    public static String formatCurrency(double amount, String currencyCode){
+        NumberFormat format=NumberFormat.getCurrencyInstance();
+        format.setCurrency(Currency.getInstance(currencyCode));
+        return format.format(amount);
+    }
+
+    public static Boolean isBusinessOpen(String openTime, String closeTime){
+        LocalDateTime now=LocalDateTime.now();
+        LocalDateTime close=LocalDateTime.parse(openTime);
+        LocalDateTime open=LocalDateTime.parse(openTime);
+
+        if(!now.isBefore(open) && !now.isAfter(close)){
+            return true;
+        }
+        return false;
+    }
+}
