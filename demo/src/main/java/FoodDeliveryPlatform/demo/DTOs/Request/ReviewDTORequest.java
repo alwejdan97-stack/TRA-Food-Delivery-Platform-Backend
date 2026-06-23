9702e5f8d7be1e9c0594c2d7745bc7557b48f70e
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-23 11:06:55 +0400

    creating Review DTO classes

--- demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Request/ReviewDTORequest.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Request/ReviewDTORequest.java
@@ -1,10 +1,26 @@
 package FoodDeliveryPlatform.demo.DTOs.Request;
 
+import FoodDeliveryPlatform.demo.Entities.DeliveryDriver;
+import FoodDeliveryPlatform.demo.Entities.Review;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class ReviewDTORequest
-{
+public class ReviewDTORequest {
+    @NotBlank(message = "Target Can't Be Empty")
+    private String targetType;
+    @Min(1) @Max(5)
+    private Integer rating;
+    @NotBlank(message = "Comment Can't Be Empty")
+    private String comment;
+
+    public void applyToEntity(Review entity){
+        entity.setTargetType(this.targetType);
+        entity.setRating(this.rating);
+        entity.setComment(this.comment);
+    }
 }
