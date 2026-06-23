4fcfc3cd8b59b504da840e72b915a64b4e229e71
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-23 09:05:27 +0400

    creating OrderItem DTO classes

--- demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Summary/OrderItemDTOSummary.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/DTOs/Summary/OrderItemDTOSummary.java
@@ -1,4 +1,32 @@
 package FoodDeliveryPlatform.demo.DTOs.Summary;
 
+import FoodDeliveryPlatform.demo.Entities.DeliveryDriver;
+import FoodDeliveryPlatform.demo.Entities.OrderItem;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class OrderItemDTOSummary {
+    @PositiveOrZero
+    private Integer quantity;
+    @DecimalMin("0.0")
+    private Double unitPrice;
+    @DecimalMin("0.0")
+    private Double totalPrice;
+    @NotBlank(message = "Instructions Can't Be Empty")
+    private String specialInstructions;
+
+    public static OrderItemDTOSummary fromEntity(OrderItem entity) {
+        OrderItemDTOSummary dto = new OrderItemDTOSummary();
+        dto.setQuantity(entity.getQuantity());
+        dto.setTotalPrice(entity.getTotalPrice());
+        dto.setUnitPrice(entity.getUnitPrice());
+        dto.setSpecialInstructions(entity.getSpecialInstructions());
+        return dto;
+    }
 }
