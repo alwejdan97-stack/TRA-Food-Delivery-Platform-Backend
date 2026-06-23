d8e87bd3ded793d9f5dd0e018c21a4f54b059ddb
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-23 16:48:29 +0400

    creating CustomerService classes

--- demo/src/main/java/FoodDeliveryPlatform/demo/Services/CustomerService.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Services/CustomerService.java
@@ -5,11 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerService {
 
-    createCustomer(CustomerRequestDTO dto)
-createCustomer(CustomerRequestDTO dto, CustomerAddressRequestDTO
-            initialAddress)
-addAddress(Integer customerId, CustomerAddressRequestDTO address)
-updateLoyaltyPoints(Integer customerId, int points)
-applyLoyaltyPenalty(Integer customerId, int pointsDeducted)
-deactivateCustomer(Integer customerId)
+    createCustomer(CustomerRequestDTO dto){}
+    createCustomer(CustomerRequestDTO dto, CustomerAddressRequestDTO initialAddress){}
+    addAddress(Integer customerId, CustomerAddressRequestDTO address){}
+    updateLoyaltyPoints(Integer customerId, int points){}
+    applyLoyaltyPenalty(Integer customerId, int pointsDeducted){}
+    deactivateCustomer(Integer customerId){}
 }
