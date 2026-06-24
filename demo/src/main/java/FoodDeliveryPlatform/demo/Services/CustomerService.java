bdf780409d342c24f495347203653c4d4b12d5c9
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-24 08:25:40 +0400

    adding CustomerService methods

--- demo/src/main/java/FoodDeliveryPlatform/demo/Services/CustomerService.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Services/CustomerService.java
@@ -1,11 +1,28 @@
 package FoodDeliveryPlatform.demo.Services;
 
+import FoodDeliveryPlatform.demo.DTOs.Request.CustomerAddressRequest;
+import FoodDeliveryPlatform.demo.DTOs.Request.CustomerDTORequest;
+import FoodDeliveryPlatform.demo.DTOs.Response.CustomerDTOResponse;
+import FoodDeliveryPlatform.demo.Entities.Customer;
+import FoodDeliveryPlatform.demo.Repositories.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+    CustomerRepository customerRepository;
+    CustomerDTORequest customerDTORequest;
+    CustomerDTOResponse customerDTOResponse;
+    CustomerAddressRequest customerAddressRequest;
+    Customer customer;
+    public static List<Customer> customers;
 
-    createCustomer(CustomerRequestDTO dto){}
+
+
+    createCustomer(CustomerRequestDTO dto){
+
+    }
     createCustomer(CustomerRequestDTO dto, CustomerAddressRequestDTO initialAddress){}
     addAddress(Integer customerId, CustomerAddressRequestDTO address){}
     updateLoyaltyPoints(Integer customerId, int points){}
