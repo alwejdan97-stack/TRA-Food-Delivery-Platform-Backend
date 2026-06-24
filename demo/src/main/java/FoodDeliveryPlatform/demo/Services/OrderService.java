561b13d1a3f558c8db8008e93c9e82f8adb43403
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-24 15:39:33 +0400

    adding methods in OrderService class

--- demo/src/main/java/FoodDeliveryPlatform/demo/Services/OrderService.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Services/OrderService.java
@@ -1,4 +1,24 @@
 package FoodDeliveryPlatform.demo.Services;
 
+import FoodDeliveryPlatform.demo.DTOs.Request.CorporateOrderRequestDTO;
+import FoodDeliveryPlatform.demo.DTOs.Request.OrderItemRequestDTO;
+import FoodDeliveryPlatform.demo.DTOs.Request.OrdersRequestDTO;
+import FoodDeliveryPlatform.demo.DTOs.Response.OrdersResponseDTO;
+import FoodDeliveryPlatform.demo.Repositories.MenuItemRepository;
+import FoodDeliveryPlatform.demo.Repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+    OrderRepository orderRepository;
+    MenuItemRepository menuItemRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository, MenuItemRepository menuItemRepository) {
+        this.orderRepository = orderRepository;
+        this.menuItemRepository = menuItemRepository;
+    }
+
+
 }
