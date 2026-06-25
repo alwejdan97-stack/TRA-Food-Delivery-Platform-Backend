8d458d3143fc21fdb1e73d75a8ca1ffcd85caf97
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-25 10:46:40 +0400

    adding DeliveryService class

--- demo/src/main/java/FoodDeliveryPlatform/demo/Services/DeliveryService.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Services/DeliveryService.java
@@ -1,4 +1,18 @@
 package FoodDeliveryPlatform.demo.Services;
 
+import FoodDeliveryPlatform.demo.Repositories.CustomerRepository;
+import FoodDeliveryPlatform.demo.Repositories.DeliveryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DeliveryService {
+    DeliveryRepository deliveryRepository;
+
+    @Autowired
+    public DeliveryService(DeliveryRepository deliveryRepository) {
+        this.deliveryRepository = deliveryRepository;
+    }
+
+
 }
