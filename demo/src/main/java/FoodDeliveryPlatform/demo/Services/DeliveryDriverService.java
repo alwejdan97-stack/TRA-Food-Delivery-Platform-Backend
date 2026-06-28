98fcc25e9d8f5aa3e18b4cf5f532a4dfe6b7e0bc
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-28 09:02:45 +0400

    adding getAllDrivers()

--- demo/src/main/java/FoodDeliveryPlatform/demo/Services/DeliveryDriverService.java
+++ demo/src/main/java/FoodDeliveryPlatform/demo/Services/DeliveryDriverService.java
@@ -1,24 +1,31 @@
 package FoodDeliveryPlatform.demo.Services;
 
 import FoodDeliveryPlatform.demo.DTOs.Request.DeliveryDriverRequestDTO;
+import FoodDeliveryPlatform.demo.DTOs.Response.DeliveryDriverResponseDTO;
 import FoodDeliveryPlatform.demo.Entities.DeliveryDriver;
 import FoodDeliveryPlatform.demo.Repositories.DeliveryDriverRepository;
 import FoodDeliveryPlatform.demo.Repositories.DeliveryRepository;
 import FoodDeliveryPlatform.demo.Utilities.HelperUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DeliveryDriverService {
     DeliveryDriverRepository driverRepository;
     DeliveryRepository deliveryRepository;
 
-    public DeliveryDriver registerDriver(DeliveryDriverRequestDTO request) {
+    public DeliveryDriverResponseDTO registerDriver(DeliveryDriverRequestDTO request) {
         DeliveryDriver newDriver = new DeliveryDriver();
         newDriver.setFirstName(request.getFirstName());
         newDriver.setLastName(request.getLastName());
         newDriver.setDriverCode(HelperUtils.generateCode("DD-01"));
         newDriver.setIsOnline(false);
 
-        return driverRepository.save(newDriver);
+        return DeliveryDriverResponseDTO.convertToDTO(driverRepository.save(newDriver));
+    }
+
+    public List<DeliveryDriverResponseDTO> getAllDrivers() {
+        return DeliveryDriverResponseDTO.convertToDTO(driverRepository.findAll());
     }
 }
